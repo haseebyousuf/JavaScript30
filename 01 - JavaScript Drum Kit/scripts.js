bbe54e6c1ac439eb56ae2997bc212299b92eb88c
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-02-06 11:12:48 +0530

    final solution done

--- 01 - JavaScript Drum Kit/scripts.js
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -1,1 +1,23 @@
-alert("hello");
+
+function playAudio(e){
+    //get the audio element to play sound
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    //get the key to show transition
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add("playing");
+}
+
+//listen for key event to play audio
+window.addEventListener('keydown',playAudio);
+
+function removeTransition(e){
+    if(e.propertyName !== "transform") return; //skip if the element is not transformed
+    e.currentTarget.classList.remove("playing"); 
+}
+
+//listen for transitionend event to remove the transition from the keys
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
